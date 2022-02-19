4f2f16e98b8ec672831525d7191bba8dc4e56e46
sieva-cmd <musyokasieva@gmail.com>
2022-02-19 20:33:55 +0300

    add functions to that will help in creating credentials

---
+++ run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.8
+from user import User
+from user import Credentials
+
+def create_new_user(username,password):
+    """
+    method to create new user with username and password
+    """
+    new_user = User(username,password)
+    return new_user
+def save_user(user):
+    """method to save new user
+    
+    """
+    user.save_user()
+def display_user():
+    """
+    method to display new-user
+    """
+    return User.display_user()    
+def login_user(username,password):
+    """
+    method that checks whether user exists and then logs in
+    """
+    check_user =Credentials.verify_user(username,password)
+    return check_user
+def create_new_credentials(account,userName,password):
+    """
+    cretae new credentials to credential list
+    """
+    new_credential =Credentials(account,userName,password)
+    return new_credential
+def save_credential(credential):
+    """
+    save new credential to credential list
+    """
+    credential.save_details()
+def display_account_details():
+    """
+    diplay all saved credentials
+    """ 
+    return Credentials.display_crdentials   
+    
+def delete_credentials(credentials):
+    """
+    delete credentials from credential list
+    """
+    credentials.delete_credentials()
+        
+    
